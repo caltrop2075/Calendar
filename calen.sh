295a5c41aabc4f6b560940175dcf189a6eb8c595
Liz <31071746+caltrop2075@users.noreply.github.com>
2022-10-24 12:52:14 -0400

    Update calen.sh

--- calen.sh
+++ calen.sh
@@ -2,25 +2,38 @@
 #------------------------------------------------------------------------------#
 #                            Programmed By Liz                                 #
 #------------------------------------------------------------------------------#
+# 2022-10-24
+# updated for 'calendar' error
+#
 # calendar stuff - 6 month calendar, events, & food pantry
 #
-# even though cal turns off hilite for non-tty some code is still there
+# even though 'cal' turns off hilite for non-tty some code is still there
 # have to filter out funky codes with 'cal.sed'
-# otherwise awk oputput gets wonky
+# otherwise awk output gets wonky
 #
+# there is a glitch in 'calendar'
+# -B includes the day after today as well
+# so the date must be forced 2 days prior
+#
+ofs=2                                                    # error offset
 pre=9                                                    # pre event days
 day=$(date +"%e")
 mon=$(date +"%b")
 grn="\033[2;32m"
 non="\033[0m"
+unset LC_ALL
 
 clear
 title-80.sh -t line "Calendar & Events For The Next 30 Days & pre $pre"
 
 cal -B 1 -A 4 | cal.sed | calen.awk -v d="$day" -v t=0   # cal mode
+
+# ----------------------------------------------------------------------- before
+bfr=$(date -d @$(($(date +"%s")-(ofs*24*60*60))) +"%F")  # before n days
+bfr=${bfr//-/}                                           # remove '-'
 echo -ne "$grn"                                          # dim pre events
-calendar -w -B "$pre" > ~/temp/temp                      # remove last event
-head -n -1 ~/temp/temp | calen.awk -v t=1                # calendar mode
+calendar -t $bfr -w -B $((9-ofs)) | calen.awk -v t=1     # calendar mode
 echo -ne "$non"
+# ---------------------------------------------------------------- today & after
 calendar -w -l 30 | calen.awk -v t=1                     # calendar mode
 # pantry.sh | calen.awk -v t=2                           # other mode 2
